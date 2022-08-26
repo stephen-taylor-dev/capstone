ab93bc445ff63c543312c84132145fe00b0ce6ce
Stephen Taylor <stephen@Stephens-iMac.local>
2022-08-26 12:17:59 -0400

    current timer progress

--- capstone/benedict_option/static/benedict_option/countdown.js
+++ capstone/benedict_option/static/benedict_option/countdown.js
@@ -1,78 +1,109 @@
+// Test Timer
+var timer;
 
+            var timerCurrent;
 
-// change play button either to start or pause 
-function switch_button() {
-    if (document.getElementById("start").innerText == "Pause") {
-        document.getElementById("start").className = "btn btn-success";
-        document.getElementById("start").innerText = "Start"; 
-    } else {
-        document.getElementById("start").className = "btn btn-warning";
-        document.getElementById("start").innerText = "Pause"; 
-    }
-}
+            var timerFinish;
 
+            var timerSeconds;
 
-// reset to default values
-function reset() {
-    clearInterval(interval);
-    switch_button();
-    counter = 0; 
-    interval = -1;
-    document.getElementById("countdown").innerHTML = '';
-}
+            function drawTimer(percent){
 
-// Function for handling min and sec properly
-function convert_time(s) {
-    // trunc https://stackoverflow.com/questions/7641818/how-can-i-remove-the-decimal-part-from-javascript-number // 
-    // Math to convert secs to minutes. Divide secs by minutes and remove decimal. 
-    var min = Math.trunc(s / 60);
-    // find secs as remainder 
-    var sec = s % 60;
-    // https://stackoverflow.com/questions/8935414/getminutes-0-9-how-to-display-two-digit-numbers//
-    // format countdown display to have 2 digits.
-    return ((min < 10) ? '0' : '') + min + ':' + ((sec < 10) ? '0' : '') + sec;
-} 
+                $('div.timer').html('<div class="percent"></div><div id="slice"'+(percent > 50?' class="gt50"':'')+'><div class="pie"></div>'+(percent > 50?'<div class="pie fill"></div>':'')+'</div>');
 
+                var deg = 360/100*percent;
 
-// create counter variable to count number of secs. elapsed
-var counter = 0;
-var interval = -1;
+                $('#slice .pie').css({
 
+                    '-moz-transform':'rotate('+deg+'deg)',
 
-// This function executes every time the start/pause button is clicked.
-function timer() {
-    // convert session min to sec because count_down() will run every sec.
-    var minutes = document.getElementById("set_time").value * 60;
-    var time = document.getElementById("countdown");
-    // Check if timer is paused. If so start count_down again.
-    if (interval == -1) {
-        // start the countdown interval, calls timer function every sec.
-        interval = setInterval(count_down, 1000);
-        switch_button();
-    
-        // Intiates the countdown timer
-        function count_down() {
-            counter++; 
-            // update the countdown display after subtracting min-counter
-            time.innerHTML = convert_time(minutes - counter);
-            // if the counter = remaining timer has finished
-            if (counter == minutes) {
-                clearInterval(interval);
-                counter = 0;
-                time.innerHTML = "Break Time! &#127881;"; 
-                // update time in minute input to default to 5 for 5 min break
-                document.getElementById("set_time").innerHTML = 5;
-                // play alarm sound
-                document.getElementById("alarm").play();
-            
+                    '-webkit-transform':'rotate('+deg+'deg)',
+
+                    '-o-transform':'rotate('+deg+'deg)',
+
+                    'transform':'rotate('+deg+'deg)'
+
+                });
+
+                $('.percent').html(Math.round(percent)+'%');
+
             }
-        }
-  
-        // Else pause the timer    
-    } else {
-        clearInterval(interval);
-        interval = -1;
-        switch_button();
-    }  
-   
-}
+
+            function stopWatch(){
+
+                var seconds = (timerFinish-(new Date().getTime()))/1000;
+
+                if(seconds <= 0){
+
+                    drawTimer(100);
+
+                    clearInterval(timer);
+
+                    $('input[type=button]#watch').val('Start');
+
+                    alert('Finished counting down from '+timerSeconds);
+
+                }else{
+
+                    var percent = 100-((seconds/timerSeconds)*100);
+
+                    drawTimer(percent);
+
+                }
+
+            }
+
+            $(document).ready(function(){
+
+                $('input[type=button]#percent').click(function(e){
+
+                    e.preventDefault();
+
+                    drawTimer($('input[type=text]#percent').val());
+
+                });
+
+                $('input[type=button]#size').click(function(e){
+
+                    e.preventDefault();
+
+                    $('.timer').css('font-size',$('input[type=text]#size').val()+'px');
+
+                });
+
+                $('input[type=button]#watch').click(function(e){
+
+                    e.preventDefault();
+
+                    if($('input[type=button]#watch').val() == 'Start'){
+
+                        $('input[type=button]#watch').val('Stop');
+
+                        timerSeconds = $('input[type=text]#watch').val();
+
+                        timerCurrent = 0;
+
+                        timerFinish = new Date().getTime()+(timerSeconds*1000);
+
+                        timer = setInterval('stopWatch()',50);
+
+                    }else{
+
+                        $('input[type=button]#watch').val('Start');
+
+                        clearInterval(timer);
+
+                    }
+
+                });
+
+                $('input[type=button]#watch').click();
+
+            });
+
+
+
+
+
+
+
