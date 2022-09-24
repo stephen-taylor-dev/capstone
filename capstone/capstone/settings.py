8f68e9b7fbbd9aae9ecafd8efa3743958efd4543
Stephen Taylor <stephen@viztech360.com>
2022-09-24 18:05:12 -0400

    one mroe time

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,7 +88,7 @@ DATABASES = {
 }
 
 
-STATIC_ROOT = Path.os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
