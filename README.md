89c1c34a13532aa60c7779a0e0532d0ec52e3ecd
Stephen Taylor <stephen@viztech360.com>
2022-09-28 18:48:39 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# capstone
+# Benedict Option
+
+### A communal-liturgical prayer app
+
+This is the source code for my Computer Science Senior Capstone project at Sattler College.
+
+I built the app using the Python Django web framework. 
+
+The front end I designed with JavaScript and Bootstrap.
+
+The project is hosted on Heroku at, 
+https://benedict-option.herokuapp.com/
+
+
+
--- benedict_option/load_database.py
+++ benedict_option/load_database.py
@@ -42,11 +42,61 @@ E = Liturgy.objects.create(
     length= 2
 )
 
+F = Liturgy.objects.create(
+    author='David',
+    text="1 O Lord, rebuke me not in thine anger, neither chasten me in thy hot displeasure.\n\n2 Have mercy upon me, O Lord; for I am weak: O Lord, heal me; for my bones are vexed.\n\n3 My soul is also sore vexed: but thou, O Lord, how long?\n\n4 Return, O Lord, deliver my soul: oh save me for thy mercies' sake.\n\n5 For in death there is no remembrance of thee: in the grave who shall give thee thanks?\n\n6 I am weary with my groaning; all the night make I my bed to swim; I water my couch with my tears.\n\n7 Mine eye is consumed because of grief; it waxeth old because of all mine enemies.\n\n8 Depart from me, all ye workers of iniquity; for the Lord hath heard the voice of my weeping.\n\n9 The Lord hath heard my supplication; the Lord will receive my prayer.\n\n10 Let all mine enemies be ashamed and sore vexed: let them return and be ashamed suddenly.",
+    title='Psalm 6',
+    type='Psalm',
+    length= 2
+)
+
+
+G = Liturgy.objects.create(
+    author='David',
+    text="1 O Lord my God, in thee do I put my trust: save me from all them that persecute me, and deliver me:\n\n2 Lest he tear my soul like a lion, rending it in pieces, while there is none to deliver.\n\n3 O Lord my God, If I have done this; if there be iniquity in my hands;\n\n4 If I have rewarded evil unto him that was at peace with me; (yea, I have delivered him that without cause is mine enemy:)\n\n5 Let the enemy persecute my soul, and take it; yea, let him tread down my life upon the earth, and lay mine honour in the dust. Selah.\n\n6 Arise, O Lord, in thine anger, lift up thyself because of the rage of mine enemies: and awake for me to the judgment that thou hast commanded.\n\n7 So shall the congregation of the people compass thee about: for their sakes therefore return thou on high.\n\n8 The Lord shall judge the people: judge me, O Lord, according to my righteousness, and according to mine integrity that is in me.\n\n9 Oh let the wickedness of the wicked come to an end; but establish the just: for the righteous God trieth the hearts and reins.\n\n10 My defence is of God, which saveth the upright in heart.\n\n11 God judgeth the righteous, and God is angry with the wicked every day.\n\n12 If he turn not, he will whet his sword; he hath bent his bow, and made it ready.\n\n13 He hath also prepared for him the instruments of death; he ordaineth his arrows against the persecutors.\n\n14 Behold, he travaileth with iniquity, and hath conceived mischief, and brought forth falsehood.\n\n15 He made a pit, and digged it, and is fallen into the ditch which he made.\n\n16 His mischief shall return upon his own head, and his violent dealing shall come down upon his own pate.\n\n17 I will praise the Lord according to his righteousness: and will sing praise to the name of the Lord most high.",
+    title='Psalm 7',
+    type='Psalm',
+    length= 4
+)
+
+
+H = Liturgy.objects.create(
+    author='David',
+    text="1 O Lord, our Lord, how excellent is thy name in all the earth! who hast set thy glory above the heavens.\n\n2 Out of the mouth of babes and sucklings hast thou ordained strength because of thine enemies, that thou mightest still the enemy and the avenger.\n\n3 When I consider thy heavens, the work of thy fingers, the moon and the stars, which thou hast ordained;\n\n4 What is man, that thou art mindful of him? and the son of man, that thou visitest him?\n\n5 For thou hast made him a little lower than the angels, and hast crowned him with glory and honour.\n\n6 Thou madest him to have dominion over the works of thy hands; thou hast put all things under his feet:\n\n7 All sheep and oxen, yea, and the beasts of the field;\n\n8 The fowl of the air, and the fish of the sea, and whatsoever passeth through the paths of the seas.\n\n9 O Lord our Lord, how excellent is thy name in all the earth!",
+    title='Psalm 8',
+    type='Psalm',
+    length= 1
+)
+
+I = Liturgy.objects.create(
+    author='David',
+    text="1 I will praise thee, O Lord, with my whole heart; I will shew forth all thy marvellous works.\n\n2 I will be glad and rejoice in thee: I will sing praise to thy name, O thou most High.\n\n3 When mine enemies are turned back, they shall fall and perish at thy presence.\n\n4 For thou hast maintained my right and my cause; thou satest in the throne judging right.\n\n5 Thou hast rebuked the heathen, thou hast destroyed the wicked, thou hast put out their name for ever and ever.\n\n6 O thou enemy, destructions are come to a perpetual end: and thou hast destroyed cities; their memorial is perished with them.\n\n7 But the Lord shall endure for ever: he hath prepared his throne for judgment.\n\n8 And he shall judge the world in righteousness, he shall minister judgment to the people in uprightness.\n\n9 The Lord also will be a refuge for the oppressed, a refuge in times of trouble.\n\n10 And they that know thy name will put their trust in thee: for thou, Lord, hast not forsaken them that seek thee.\n\n11 Sing praises to the Lord, which dwelleth in Zion: declare among the people his doings.\n\n12 When he maketh inquisition for blood, he remembereth them: he forgetteth not the cry of the humble.\n\n13 Have mercy upon me, O Lord; consider my trouble which I suffer of them that hate me, thou that liftest me up from the gates of death:\n\n14 That I may shew forth all thy praise in the gates of the daughter of Zion: I will rejoice in thy salvation.\n\n15 The heathen are sunk down in the pit that they made: in the net which they hid is their own foot taken.\n\n16 The Lord is known by the judgment which he executeth: the wicked is snared in the work of his own hands. Higgaion. Selah.\n\n17 The wicked shall be turned into hell, and all the nations that forget God.\n\n18 For the needy shall not always be forgotten: the expectation of the poor shall not perish for ever.\n\n19 Arise, O Lord; let not man prevail: let the heathen be judged in thy sight.\n\n20 Put them in fear, O Lord: that the nations may know themselves to be but men. Selah.",
+    title='Psalm 9',
+    type='Psalm',
+    length= 10
+)
+
+J = Liturgy.objects.create(
+    author='David',
+    text="1 Why standest thou afar off, O Lord? why hidest thou thyself in times of trouble?\n\n2 The wicked in his pride doth persecute the poor: let them be taken in the devices that they have imagined.\n\n3 For the wicked boasteth of his heart's desire, and blesseth the covetous, whom the Lord abhorreth.\n\n4 The wicked, through the pride of his countenance, will not seek after God: God is not in all his thoughts.\n\n5 His ways are always grievous; thy judgments are far above out of his sight: as for all his enemies, he puffeth at them.\n\n6 He hath said in his heart, I shall not be moved: for I shall never be in adversity.\n\n7 His mouth is full of cursing and deceit and fraud: under his tongue is mischief and vanity.\n\n8 He sitteth in the lurking places of the villages: in the secret places doth he murder the innocent: his eyes are privily set against the poor.\n\n9 He lieth in wait secretly as a lion in his den: he lieth in wait to catch the poor: he doth catch the poor, when he draweth him into his net.\n\n10 He croucheth, and humbleth himself, that the poor may fall by his strong ones.\n\n11 He hath said in his heart, God hath forgotten: he hideth his face; he will never see it.\n\n12 Arise, O Lord; O God, lift up thine hand: forget not the humble.\n\n13 Wherefore doth the wicked contemn God? he hath said in his heart, Thou wilt not require it.\n\n14 Thou hast seen it; for thou beholdest mischief and spite, to requite it with thy hand: the poor committeth himself unto thee; thou art the helper of the fatherless.\n\n15 Break thou the arm of the wicked and the evil man: seek out his wickedness till thou find none.\n\n16 The Lord is King for ever and ever: the heathen are perished out of his land.\n\n17 Lord, thou hast heard the desire of the humble: thou wilt prepare their heart, thou wilt cause thine ear to hear:\n\n18 To judge the fatherless and the oppressed, that the man of the earth may no more oppress.",
+    title='Psalm 10',
+    type='Psalm',
+    length= 10
+)
+
+
 A.save()
 B.save()
 C.save()
 D.save()
 E.save()
+
+F.save()
+G.save()
+H.save()
+I.save()
+J.save()
+
 
 item = Group.objects.create(
     name="Public"
